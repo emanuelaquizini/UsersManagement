13055a99f6ae75e69de0713157ea25147bc74bc1
Emanuela Quizini <manu.biq@gmail.com>
2024-10-17 18:01:02 -0300

    Feat: add users route and cors policy

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
