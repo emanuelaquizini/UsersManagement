0944001a6ae7db7601dc27b0994e0121e1fcb33e
Emanuela Quizini <manu.biq@gmail.com>
2024-10-17 10:42:58 -0300

    Feat: adicionar user controller

--- UserManagementApi/Controllers/UsersController.cs
+++ UserManagementApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UserManagementApi.Data;
@@ -6,9 +7,9 @@ using UserManagementApi.Models;
 
 namespace UserManagementApi.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
+
     public class UsersController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -18,6 +19,20 @@ namespace UserManagementApi.Controllers
             _context = context;
         }
 
-      
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var result = await _context.Users.ToListAsync();
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
+        }
+
     }
 }
